acc80c63d74dc9268d8e2cdb4cf9de116767ee4d
Anurag Ghosh <ghoshanurag1995@gmail.com>
2015-12-05 23:59:10 +0530

    Minor changes

--- create_hash_keys.m
+++ create_hash_keys.m
@@ -4,6 +4,14 @@
 % while also finding the set of keys for the points.
 function [func, keySet] = create_hash_keys(valueSet, w, b)
 pca_vector_num = 2;
-func = pcasecon(valueSet.',pca_vector_num); 
-keySet = (valueSet*func + b)/w;
+func = pcasecon(valueSet.',pca_vector_num);
+set_val = (valueSet*func + b)/w;
+keySet = byte_concat(1/set_val(:,1),1/set_val(:,2));
 % FUNCTION create_hash_keys END
+
+function [z] = byte_concat(h1,h2)
+x=uint8(h1); % first byte
+y=uint8(h2); % second byte
+bytepack=uint16(x);
+bytepack=bitshift(bytepack,8);
+z=bitor(bytepack,uint16(y));
